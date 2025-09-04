cec67bf3c2aa6d8f4973f61b848e39e590656d7b
Volodymyr Dedenko <dedenkof@ukr.net>
2025-09-04 21:47:47 +0300

    Add ngClass Core Directive

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
 import { Course } from "../model/course";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: "course-card",
   templateUrl: "./course-card.component.html",
-  styleUrls: ["./course-card.component.css"]
+  styleUrls: ["./course-card.component.css"],
+	imports: [CommonModule]
 })
 export class CourseCardComponent implements OnInit{
 
@@ -18,6 +20,14 @@ export class CourseCardComponent implements OnInit{
 
 	onCourseViwed () {	
 		this.courseSelected.emit(this.course);
+	}
+
+	cardClasses() {
+		//return {'beginner': this.course.category == 'BEGINNER'}
+		if(this.course.category == 'BEGINNER') {
+			//return ['beginner'];
+			return 'beginner';
+		}
 	}
 
 	ngOnInit () {}
