361b38fe7c73b9e17da758925c3a354295add4d8
Volodymyr Dedenko <dedenkof@ukr.net>
2025-09-19 18:19:34 +0300

    implements ng-content content projection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,13 +23,6 @@ import { CommonModule } from "@angular/common";
 export class AppComponent implements AfterViewInit {
   courses = COURSES;
 
-  //@ViewChild(CourseCardComponent) card1: CourseCardComponent;
-
-  //@ViewChild("cardRef", { read: ElementRef }) card: CourseCardComponent;
-
-  // @ViewChild('container') containerDiv: ElementRef;
-
-  //@ViewChild("courseImage") courseImage: ElementRef;
   
 	@ViewChildren(CourseCardComponent, {read: ElementRef}) cards: QueryList<ElementRef>;
 
@@ -39,45 +32,11 @@ export class AppComponent implements AfterViewInit {
 
   ngAfterViewInit() {
 
-		// this.cards.changes.subscribe(
-		// 	cards => console.log(cards)
-		// );
-
 		console.log(this.cards);
-
-
-    //console.log("courseImage", this.courseImage);
-    //console.log(this.cards.first);
   }
 
   onCourseSelected(course: Course) {
     //console.log("containerDiv", this.card);
   }
 
-	onCoursesEdited() {
-		this.courses.push({
-        id: 1,
-        description: "Angular Core Deep Dive",
-        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
-        longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
-        category: 'INTERMEDIATE',
-        lessonsCount: 10
-    }
-	)
-	}
-
-  // startDate = new Date(2025, 9, 5);
-
-  // title = COURSES[0].description;
-
-  // price = 9.996587989;
-
-  // rate = 0.67;
-
-  // course = COURSES[0];
-
-  // tracking loop for function insert into for track instead course.id (not use our example not suit on)
-  // trackCourse(index:number, course:Course) {
-  // 	return course.id;
-  // }
 }
